e5989eb81e6cc5e22d3bff4a657a5cbc62909864
robbiestoffel <robbiestoffel@gmail.com>
2021-08-02 08:15:47 -0400

    change prefix

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,7 +1,7 @@
 const cooldowns = new Map();
 
 module.exports = (Discord, client, message) => {
-    const prefix = '$';
+    const prefix = 'penguin ';
     if(!message.content.startsWith(prefix) || message.author.bot) return;
     
     const args = message.content.slice(prefix.length).split(/ +/);
