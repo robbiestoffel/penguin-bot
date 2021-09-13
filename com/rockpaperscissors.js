4bc736bc553f70a48673cbe1d71c5e0eca837ecc
robbiestoffel <robbiestoffel@gmail.com>
2021-09-13 10:06:36 -0400

    preping for penguin event, rock paper scissors!

---
+++ com/rockpaperscissors.js
@@ -0,0 +1,57 @@
+module.exports = {
+    name: 'rockpaperscissors',
+    aliases: ['rps'],
+    permissions: [],
+    cooldowns: 10,
+    description: 'Play Rock Paper Scissors',
+    async execute(message, args, cmd, client, Discord) {
+        if(!args[0] || !args[1]) return message.channel.send('Please tag the two people that are playing');
+
+        const channel = await message.guild.channels.create(`choose rock, paper, or scissors here ${message.author.tag}`);
+        channel.setParent('886569387203629106');
+
+        channel.updateOverwrite(message.guild.id, {
+            SEND_MESSAGES: false,
+            VIEW_CHANNEL: false
+        });
+        channel.updateOverwrite(message.author, {
+            SEND_MESSAGES: true,
+            VIEW_CHANNEL: true
+        });
+
+        const reactionMessage = await channel.send('Thank you for contacting support!');
+
+        try{
+            
+            await reactionMessage.react(":watermelon:");
+            await reactionMessage.react(":eyes:");
+        }catch(err){
+            channel.send("Error...")
+            throw err;
+        }
+
+        const collector = reactionMessage.createReactionCollector((reaction, user) =>
+            message.guild.members.cache.find((member) => member.id === user.id).hasPermission('ADMINISTRATOR'),
+            { dispose: true }
+        );
+
+        collector.on('collect', (reaction, user) => {
+            switch (reaction.emoji.name){
+                case "":
+                    channel.updateOverwrite(message.author, { SEND_MESSAGES: false});
+                    break;
+                case "":
+                    channel.send("Deleting this channel in 5 seconds!");
+                    setTimeout(() => channel.delete(), 5000);
+                    break;
+            }
+        });
+
+        message.channel.send('We will be right with you! ${channel}').then((msg) => {
+            setTimeout(() => msg.delete(), 7000);
+            setTimeout(() => message.delete(), 3000);
+        }).catch((err) => {
+            throw err;
+        });
+    }
+}
